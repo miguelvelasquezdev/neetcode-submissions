6126bd695d096254851489335f581087a1895fad
Miguel Velasquez <veruiz.miguel@gmail.com>
2026-06-15 20:25:52 -0500

    Add: last-stone-weight - submission-1

---
+++ Data Structures & Algorithms/last-stone-weight/submission-1.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number[]} stones
+     * @return {number}
+     */
+    lastStoneWeight(stones) {
+        const newStones = [...stones]
+        while (newStones.length > 1) {
+            newStones.sort((a, b) => a - b)
+            if (newStones[newStones.length - 2] === newStones[newStones.length - 1]) {
+                newStones.pop()
+                newStones.pop()
+            } else if (newStones[newStones.length - 2] > newStones[newStones.length - 1]) {
+                const res = newStones[newStones.length - 2] - newStones[newStones.length - 1]
+                newStones.pop()
+                newStones.pop()
+                newStones.push(res)
+            } else {
+                const res = newStones[newStones.length - 1] - newStones[newStones.length - 2]
+                newStones.pop()
+                newStones.pop()
+                newStones.push(res)
+            }
+        }
+
+        if (newStones.length) return newStones[0]
+        return 0
+    }
+}
