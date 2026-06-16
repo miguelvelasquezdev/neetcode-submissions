0c836f899df64900bd2f1e749a093df6e443917f
Miguel Velasquez <veruiz.miguel@gmail.com>
2026-06-15 20:11:05 -0500

    Add: kth-largest-integer-in-a-stream - submission-1

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-1.js
@@ -0,0 +1,36 @@
+class KthLargest {
+    /**
+     * @param {number} k
+     * @param {number[]} nums
+     */
+    constructor(k, nums) {
+        this.k = k
+        this.nums = nums
+        console.log(k , nums)
+    }
+
+    /**
+     * @param {number} val
+     * @return {number}
+     */
+    add(val) {
+        if (typeof val !== 'number') return null
+
+        console.log(this.nums, 'nums???')
+
+        this.nums.push(val)
+        const numsSorted = [...this.nums].sort((a, b) => a - b)
+        console.log(numsSorted, 'numsSorted')
+
+        return numsSorted[numsSorted.length - this.k]
+    }
+}
+
+/**
+ * [1, 2h, 3, 3] "2"
+ * [1, 2, 3h, 3, 3] 3
+ * [1, 2, 3, 3h, 3, 5] 3
+ * [1, 2, 3, 3, 3h, 5, 6] 3
+ * [1, 2, 3, 3, 3, 5h, 6, 7] 5
+ * [1, 2, 3, 3, 3, 5, 6h, 7, 8] 6
+ */
